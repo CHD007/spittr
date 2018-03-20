fd3208c6dc50d5b912ef8c49d3b1cf34aeb14c89
chd007 <chernyshov777@gmail.com>
2018-03-20 20:10:19 +0300

    spittle impl

---
+++ src/main/java/spitter/data/SpittleRepositoryImpl.java
@@ -0,0 +1,23 @@
+package spitter.data;
+
+import org.springframework.stereotype.Component;
+import spitter.Spittle;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Component
+public class SpittleRepositoryImpl implements SpittleRepository {
+    @Override
+    public List<Spittle> findSpittles(long max, int count) {
+        return createSpittlesList(count);
+    }
+
+    private List<Spittle> createSpittlesList(int count) {
+        return IntStream.range(0, count)
+                .mapToObj(i -> new Spittle("Spittle " + i, new Date()))
+                .collect(Collectors.toList());
+    }
+}
