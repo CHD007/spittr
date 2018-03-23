4eee6bd098bc283c3adc7f047a96781358f7835b
chd007 <chernyshov777@gmail.com>
2018-03-23 16:18:11 +0300

    add javadoc for SpittleRepository.java

--- src/main/java/spitter/data/SpittleRepository.java
+++ src/main/java/spitter/data/SpittleRepository.java
@@ -4,6 +4,19 @@ import spitter.Spittle;
 
 import java.util.List;
 
+/**
+ * The repository that contains spittles.
+ *
+ * @author dchernyshov
+ */
 public interface SpittleRepository {
+
+    /**
+     * Find spittles in repository
+     *
+     * @param max   max id of returned spittles
+     * @param count count of returned spittles
+     * @return      list of spittles
+     */
     List<Spittle> findSpittles(long max, int count);
 }
