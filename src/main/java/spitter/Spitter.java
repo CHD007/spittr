7619923d4ab67936b3ef652735488b04ef355605
chd007 <chernyshov777@gmail.com>
2018-03-25 22:38:27 +0300

    registration validation

--- src/main/java/spitter/Spitter.java
+++ src/main/java/spitter/Spitter.java
@@ -2,6 +2,7 @@ package spitter;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
@@ -12,18 +13,22 @@ public class Spitter {
     private Long id;
 
     @NonNull
+    @NotNull
     @Size(min = 5, max = 16)
     private String userName;
 
     @NonNull
+    @NotNull
     @Size(min = 5, max = 25)
     private String password;
 
     @NonNull
+    @NotNull
     @Size(min =2, max = 30)
     private String firstName;
 
     @NonNull
+    @NotNull
     @Size(min = 2, max = 30)
     private String lastName;
 }
--- src/main/java/spitter/web/SpitterController.java
+++ src/main/java/spitter/web/SpitterController.java
@@ -3,11 +3,14 @@ package spitter.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import spitter.Spitter;
 import spitter.data.SpitterRepository;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "/spitter")
@@ -29,7 +32,12 @@ public class SpitterController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String proccessRegistration(Spitter spitter) {
+    public String proccessRegistration(
+            @Valid Spitter spitter, Errors errors) {
+        if (errors.hasErrors()) {
+            return "registerForm";
+        }
+
         spitterRepository.save(spitter);
 
         return "redirect:/spitter/" + spitter.getUserName();
