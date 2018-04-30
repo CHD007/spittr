c921b2feb1188337c19b85d8727246150ed8fe99
chd007 <chernyshov777@gmail.com>
2018-04-30 19:10:06 +0300

    spring caching

---
+++ src/main/java/spitter/config/CacheConfig.java
@@ -0,0 +1,29 @@
+package spitter.config;
+
+import net.sf.ehcache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Enabling Spring's support for annotation-driven caching.
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public EhCacheCacheManager cacheManager(CacheManager cacheManager) {
+        return new EhCacheCacheManager(cacheManager);
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehcache() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("/ehcache.xml"));
+        return ehCacheManagerFactoryBean;
+    }
+}
--- src/main/java/spitter/data/SpitterRepository.java
+++ src/main/java/spitter/data/SpitterRepository.java
@@ -1,5 +1,6 @@
 package spitter.data;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spitter.domain.Spitter;
 
@@ -9,5 +10,15 @@ import spitter.domain.Spitter;
  * @author dchernyshov
  */
 public interface SpitterRepository extends JpaRepository<Spitter, Long> {
+    /**
+     * Find spitter by username.
+     *
+     * To check that return value is cached and this method calls only once turn on hibernate sql log
+     * and check that it goes to database only once.
+     *
+     * @param userName username of spitter to be find
+     * @return spitter by username
+     */
+    @Cacheable("spittleCache")
     Spitter findByUserName(String userName);
 }
